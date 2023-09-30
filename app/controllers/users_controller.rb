1f1bb248a6d00fddb5f15ce40ed43bebe488d991
下辻佑太 <apple@yutaMacBook-Air.local>
2023-09-30 17:25:07 +0900

    show開通

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,21 @@
 class UsersController < ApplicationController
-  
+
+  before_action :set_user, only: [:show]
+
   def top
   end
   
   def index
   end
+
+  def show
+    @ntt = "ちんこ"
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: 'users#top'
 
   get 'users/index'
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
